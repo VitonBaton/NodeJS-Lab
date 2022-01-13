9ca415013832978ad4dd3c1f318821388e2e1070
VitonBaton <vitonbaton@yandex.ru>
2022-01-13 18:48:46 +0300

    add swagger doc

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ const app = express();
 const loader = require('./src/loader');
 const port = require('./src/env').app.port;
 
+const swaggerUI = require('swagger-ui-express');
+const swaggerDoc = require('./src/swagger/openapi.json');
+
 async function start() {
     await database.connect();
+
     app.use(loader);
+    app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
+
     app.listen(port, () => console.log(`Server successfully started at port ${port}`));
 }
 
